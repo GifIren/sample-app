fc77287f961ba4c14d314b4456928829c032c289
Irene Ikpo <Irene.Ikpo@Interswitchng.com>
2023-08-18 12:31:31 +0100

    Second Commit

---
+++ src/main/java/com/interswitch/maven/App.java
@@ -0,0 +1,15 @@
+package com.interswitch.maven;
+
+
+import java.util.Scanner;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args){}
+
+
+}
---
+++ src/main/java/com/interswitch/maven/SimpleCalculator.java
@@ -0,0 +1,31 @@
+package com.interswitch.maven;
+
+public class SimpleCalculator {
+
+public static double add(double num1, double num2){
+    return num1 + num2;
+}
+
+public static double subtract(double num1, double num2){
+    return num1 - num2;
+}
+public static double multiply(double num1, double num2){
+    return num1 * num2;
+}
+public static double divide(double num1, double num2){
+    if (num2 != 0) {
+        return num1/num2;
+    }
+    else {
+        throw new ArithmeticException("division by zero");
+    }
+}
+
+
+
+
+
+
+
+
+}
